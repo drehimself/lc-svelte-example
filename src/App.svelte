b078744122ed32cf91f4b70c6efe59482b020aa6
Andre Madarang <andre_madarang@hotmail.com>
2022-05-04 22:31:17 -0400

    Example

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+  export let name;
+
+  import Counter from "./Counter.svelte";
+
+  import "./style.css";
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+  <div class="hello">This is global</div>
+  <Counter title="My Counter" />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ src/Counter.svelte
@@ -0,0 +1,76 @@
+<script>
+  import { onMount } from "svelte";
+
+  export let title = "My default title";
+  let count = 0;
+  let myText = "";
+  const todos = [
+    {
+      id: 1,
+      title: "Finish Svelte Screencast",
+      isComplete: false,
+    },
+    {
+      id: 2,
+      title: "Go shopping",
+      isComplete: false,
+    },
+    {
+      id: 3,
+      title: "Take over world",
+      isComplete: false,
+    },
+  ];
+
+  $: doubled = count * 2;
+
+  $: {
+    console.log(`multiple statements can be combined`);
+    console.log(`the current count is ${count}`);
+  }
+
+  onMount(() => {
+    console.log("Counter mounted");
+  });
+
+  function decrement() {
+    count--;
+  }
+
+  function increment() {
+    count++;
+  }
+</script>
+
+<div>
+  <h2>{title}</h2>
+  <div>
+    <div>Count is {count} Doubled is {doubled}</div>
+    {#if count > 10}
+      Count is high
+    {:else if count > 5}
+      Count is in between
+    {:else}
+      Count is low
+    {/if}
+    <div>
+      <button on:click={() => count--}>Decrement</button>
+      <button on:click={increment}>Increment</button>
+    </div>
+  </div>
+  <div>
+    <form action="#">
+      <input type="text" bind:value={myText} />
+      <div>{myText}</div>
+    </form>
+  </div>
+  <div>
+    <h1>Each</h1>
+    <div class="hello">This is global</div>
+    <ul>
+      {#each todos as { id, title }, index (id)}
+        <li>{index} {id} {title}</li>
+      {/each}
+    </ul>
+  </div>
+</div>
